7f2de32ef78b7416ac96fdb58e4f229c2e9eacca
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-04-03 23:21:28 +0530

    Stashed

--- my_project/my_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import EmotionPrediction
-
-class EmotionPredictionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EmotionPrediction
-        fields = '_all_'
